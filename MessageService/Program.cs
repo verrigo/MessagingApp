9c544f1441f14dd46da2945e3953fca6252bf265
Radu Caprescu <verrigo@users.noreply.github.com>
2022-09-06 07:39:45 +0300

    Fixes Azure Clients Registering

--- MessageService/Program.cs
+++ MessageService/Program.cs
@@ -9,14 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddServiceBusClient(azureServiceBusConnectionString)
-    .WithName("AzureServiceBus")
-    .ConfigureOptions(options =>
-    {
-        options.RetryOptions.Delay = TimeSpan.FromMilliseconds(50);
-        options.RetryOptions.MaxDelay = TimeSpan.FromSeconds(2);
-        options.RetryOptions.MaxRetries = 3;
-    });
+var azureServiceBusConnectionString = builder.Configuration.GetConnectionString("AzureServiceBus");
+if (azureServiceBusConnectionString == null)
+    throw new Exception("Service Bus Connection String cannot be null");
+builder.Services.AddAzureClients(clientsBuilder =>
+                {
+                    clientsBuilder.AddServiceBusClient(azureServiceBusConnectionString)
+                        .WithName("AzureServiceBus")
+                        .ConfigureOptions(options =>
+                        {
+                            options.RetryOptions.Delay = TimeSpan.FromMilliseconds(50);
+                            options.RetryOptions.MaxDelay = TimeSpan.FromSeconds(2);
+                            options.RetryOptions.MaxRetries = 3;
+                        });
+                });
 builder.Services.AddScoped<AzureServiceBusTopicSender>();
 var app = builder.Build();
 
