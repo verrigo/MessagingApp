3c5258b7293630321f8f48c93cd676fe4da2928a
Radu Caprescu <verrigo@users.noreply.github.com>
2022-09-15 07:08:57 +0300

    Added gRPC service to receive messages

--- MessageService/Controllers/MessageController.cs
+++ MessageService/Controllers/MessageController.cs
@@ -9,16 +9,28 @@ namespace MessageService.Controllers
     {
         private IServiceBusSender _serviceBusSender;
         private ILogger<MessageController> _logger;
-        public MessageController(IServiceBusSender serviceBusSender, ILogger<MessageController> logger)
+        private IServiceBusMessageProcessor _serviceBusMessageProcessor;
+        public MessageController(IServiceBusMessageProcessor serviceBusMessageProcessor, IServiceBusSender serviceBusSender, ILogger<MessageController> logger)
         {
             _serviceBusSender = serviceBusSender;
             _logger = logger;
+            _serviceBusMessageProcessor = serviceBusMessageProcessor;
         }
 
-        [HttpPost(Name ="SendMessage")]
+        [HttpPost(Name = "SendMessage")]
         public async Task Send(Message message)
         {
-            await _serviceBusSender.SendMessageAsync(message);
+            await _serviceBusSender.SendMessageAsync(message).ConfigureAwait(false);
+        }
+
+        //this will not work. 
+        //Need to figure out something else;
+        //will move this to another micro service
+        //gRPC? why not
+        [HttpGet(Name = "GetMessages")]
+        public async Task<Message> Get(string receiverId)
+        {
+
         }
     }
 }
